080e86c03bc4d301328f7b9292aef4dac57bfcca
Anaid0616 <152166423+Anaid0616@users.noreply.github.com>
2024-12-10 12:49:41 +0100

    Add husky again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
+  pluginJs.configs.recommended,
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        describe: true, // Used for grouping tests
-        test: true, // Used to create tests
-        it: true, // Alternative way to create tests
-        expect: true, // Used for test assertions
-        require: true, // Used in Node.js files like Tailwind config
-        module: true, // Used in Node.js files like Tailwind config
-        process: true, // Used for environment variables later
+        module: true,
+        require: true,
+        process: true,
+        test: true,
       },
     },
   },
-  pluginJs.configs.recommended,
 ];
